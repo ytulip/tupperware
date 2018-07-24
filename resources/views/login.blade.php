e3561807fea51820dc808bf21a44cb535eb86ac2
childyi <yi@zhuyan.me>
2018-07-24 23:09:17 +0800

    input颜色！

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,6 +15,19 @@
         input:focus::-webkit-input-placeholder{
             color: transparent;
         }
+
+        ::-webkit-input-placeholder { /* WebKit browsers */
+            color:    #93989E;
+        }
+        :-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+            color:    #93989E;
+        }
+        ::-moz-placeholder { /* Mozilla Firefox 19+ */
+            color:    #93989E;
+        }
+        :-ms-input-placeholder { /* Internet Explorer 10+ */
+            color:    #93989E;
+        }
     </style>
 @stop
 @section('container')
@@ -24,7 +37,7 @@
 
     <div style="padding: 0 24px;margin-top: 76px;">
         <div>
-            <input placeholder="输入工号ID" style="display:inline-block;line-height: 18px;font-size: 17px;color: #93989E;letter-spacing: 0;background: #F7F7F9;border: 1px solid #EEEEEE;border-radius: 100px;width: 100%;text-align: center;box-sizing: border-box;padding: 13px 0;"/>
+            <input placeholder="输入工号ID" style="display:inline-block;line-height: 18px;font-size: 17px;color: #232A31;letter-spacing: 0;background: #F7F7F9;border: 1px solid #EEEEEE;border-radius: 100px;width: 100%;text-align: center;box-sizing: border-box;padding: 13px 0;"/>
         </div>
         <div style="margin-top: 32px;">
             <a class="t-al-c" style="display: inline-block;width: 100%;background: #E01885;box-shadow: 0 2px 8px 0 rgba(224,24,133,0.36);border-radius: 100px;line-height: 44px;font-size: 17px;color: #FFFFFF;letter-spacing: 0;">进入</a>
