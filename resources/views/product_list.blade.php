0c1fadf2265cfc8dddfa35cffdbbb89ef9c8ca75
zhuyan <yan.zhu01@bqjr.cn>
2019-04-12 14:15:53 +0800

    标题

--- resources/views/product_list.blade.php
+++ resources/views/product_list.blade.php
@@ -264,6 +264,8 @@
 
     </div>
 
+    <div style="margin-bottom: 100px;"></div>
+
 
 
     <div style="position: fixed;top:0;right: 0;left: 0;bottom: 0;background-color:rgba(0,0,0,.6);z-index: 999;" v-if="layerSwitch">
